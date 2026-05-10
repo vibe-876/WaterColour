e84ea484f9a552ed541d7a85a5b1c580a1229181
cam <cameronayres123@gmail.com>
2026-05-10 01:06:02 +0100

    Renamed the main file.

--- watercolour.sh
+++ watercolour.sh
@@ -3,6 +3,7 @@
 ## Ignore this push, it's a test...
 ## Edit to preference.
 scan_area="$HOME/Downloads"
-malware_list="$HOME/Programming/Projects/WaterColour/malware_list"
+malware_list="malware_list"
+
 
 find $scan_area -type f -exec sha256sum {} \; | awk '{print $1}' | grep -F -f $malware_list
